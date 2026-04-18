12d81f2113cfcc07061903b7b80867afad5d867f
Maxim <Oskolsky>
2026-04-18 21:22:47 +0300

    refactoring

--- src/components/SavedProductItem.tsx
+++ src/components/SavedProductItem.tsx
@@ -28,16 +28,17 @@ export const SavedProductItem = ({ item, onRemove }: SavedProductItemProps) => {
             </a>
 
             <div className="flex flex-1 flex-col justify-between">
-                <a
-                    href={productUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="line-clamp-1 text-sm font-medium hover:text-[#0f766e]"
-                >
-                    {item.name}
-                </a>
-
-                <div className="text-xs text-gray-400">{formatSavedAt(item.savedAt)}</div>
+                <div>
+                    <a
+                        href={productUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="line-clamp-1 text-sm font-medium hover:text-[#0f766e]"
+                    >
+                        {item.name}
+                    </a>
+                    <div className="text-xs text-gray-400">{formatSavedAt(item.savedAt)}</div>
+                </div>
 
                 <div className="flex items-center justify-between">
                     <div className="text-sm">
